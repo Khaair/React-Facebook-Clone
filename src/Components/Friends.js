e8d1385cf369420c23868b826e0308b44d737ef7
Khaair <khair.cse.iu@gmail.com>
2022-02-08 16:46:06 +0600

    facebook

--- src/Components/Friends.js
+++ src/Components/Friends.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import "../App.css";
 
  function Friends() {
   return (
     <div>
-        <h1>Friends Page</h1>
-    </div>
+    <div className="pcontainer">
+ <div className="pone">
+ 
+ </div>
+ <div className="ptwo">
+   <img className="coverphoto" src="img/coverphoto.png" alt="Nature" />
+   <h1>Under construction</h1>
+  
+ </div>
+ <div className="pthree">
+  
+ </div>
+</div>
+
+   </div>
   )
 }
 
