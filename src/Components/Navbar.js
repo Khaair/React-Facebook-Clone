7e78c78e5ce2866d401676fd04aab009c38c927b
Khair <khair.cse.iu@gmail.com>
2022-04-19 18:16:15 +0600

    API Integrate

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,76 +1,75 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "../App.css";
- function Navbar() {
+function Navbar() {
   return (
     <div>
-              <div>
+      <div>
         <div className="one">
 
-        <Link to="/" className="fouricon" >
-        <img alt="icon" src="img/fblogo.PNG" />
-            </Link>
-          
-          
+          <Link to="/" className="fouricon" >
+            <img alt="icon" src="img/fblogo.PNG" />
+          </Link>
+
+
           <input
             className="input01"
             type="text"
             placeholder="Search Facebook"
           />
-        
-            <Link to="/" className="fouricon" >
+
+          <Link to="/" className="fouricon" >
             <img alt='icon' className="homeicon" src="./img/home.PNG" />
-            </Link>
-         
-         
-            <Link to="/watch" className="ll">
-            <img alt='icon'  className="threeicon" src="img/three.PNG" />
-            </Link>
-       
-        
-            <Link to="/marketplace" className="ll">
-            <img alt='icon'  className="fouricon" src="img/four.PNG" />
-            </Link>
-       
-         
-            <Link to="/Groups" className="ll">
-            <img alt='icon'  className="fiveicon" src="img/five.PNG" /> 
-            </Link>
-        
-        
-            <Link to="/Gaming" className="ll">
-            <img alt='icon'  className="sixicon" src="img/six.PNG" />
-            </Link>
+          </Link>
 
-            <Link to="/Profile" className="ll">
+
+          <Link to="/watch" className="ll">
+            <img alt='icon' className="threeicon" src="img/three.PNG" />
+          </Link>
+
+
+          <Link to="/marketplace" className="ll">
+            <img alt='icon' className="fouricon" src="img/four.PNG" />
+          </Link>
+
+
+          <Link to="/Groups" className="ll">
+            <img alt='icon' className="fiveicon" src="img/five.PNG" />
+          </Link>
+
+
+          <Link to="/Gaming" className="ll">
+            <img alt='icon' className="sixicon" src="img/six.PNG" />
+          </Link>
+
+          <Link to="/Profile" className="ll">
             <img alt='icon' className="sevenicon" src="img/seven.PNG" />
-            </Link>
+          </Link>
 
-            <Link to="/Menu" className="ll">
-            <img alt='icon'  className="eighticon" src="img/eight.PNG" />
-            </Link>
-          
-            
+          <Link to="/Menu" className="ll">
+            <img alt='icon' className="eighticon" src="img/eight.PNG" />
+          </Link>
 
-            
 
-         
-            <Link to="/Messenger" className="ll">
-            <img alt='icon'  className="nineicon" src="./img/nine.PNG" />
-            </Link>
-        
-            <Link to="/Notification" className="ll">
-            <img alt='icon'  className="elevenicon" src="img/eleven.PNG" />
-            </Link>
-         
-            <Link to="/Account" className="ll">
-            <img alt='icon'  className="tweleveicon" src="img/tweleve.PNG" />
-            </Link>
-      
-        
-            
-      
-        </div>
+
+
+
+
+          <Link to="/Messenger" className="ll">
+            <img alt='icon' className="nineicon" src="./img/nine.PNG" />
+          </Link>
+
+          <Link to="/Notification" className="ll">
+            <img alt='icon' className="elevenicon" src="img/eleven.PNG" />
+          </Link>
+
+          <Link to="/Account" className="ll">
+            <img alt='icon' className="tweleveicon" src="img/tweleve.PNG" />
+
+
+
+        </Link>
+          </div>
       </div>
     </div>
   )
